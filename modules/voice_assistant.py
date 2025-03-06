61e5d3357228501d2db5daced5f78135135e2278
Mighty0w0Kitten <irogozin84@gmail.com>
2025-03-06 09:25:33 +0300

    Снова проблемы

--- modules/voice_assistant.py
+++
@@ -1,9 +0,0 @@
-# modules/voice_assistant.py
-class VoiceAssistant:
-    def __init__(self):
-        # Инициализация голосовых сервисов
-        pass
-
-    def handle_voice(self, command):
-        # Логика обработки голосовых команд
-        print(f"Голосовая команда: {command}")
